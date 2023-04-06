1e6e85ad5b20ff3c903e45d18d1c1f5e99642884
Ariel Stancovich <arielstancovichsuarez@gmail.com>
2023-04-05 23:12:14 -0400

    trabajo terminado

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,10 +10,6 @@ function calculo() {
     let valor1 = Number(document.getElementById("cantidadIn1").value);
     let valor2 = Number(document.getElementById("cantidadIn2").value);
     let valor3 = Number(document.getElementById("cantidadIn3").value);
-
-    // let valor1 = document.getElementById("cantidadIn1").value;
-    // let valor2 = document.getElementById("cantidadIn2").value;
-    // let valor3 = document.getElementById("cantidadIn3").value;
     let mensaje = document.getElementById("mensaje");
     let total = valor1+valor2+valor3;
     if (total<=10) {
@@ -21,5 +17,18 @@ function calculo() {
     }else{
         mensaje.innerHTML = "Llevas demaciados memes , que sean menos!";
     }
+}
+function validacion() {
+    let valor1 = document.getElementById("select1").value;
+    let valor2 = document.getElementById("select2").value;
+    let valor3 = document.getElementById("select3").value;
+    let mensajePassword = document.getElementById("mensajePassword");
 
+    if (valor1==9 && valor2==1 && valor3==1) {
+        mensajePassword.innerHTML = "password 1 correcto";
+    } else if(valor1==7 && valor2==1 && valor3==4){
+        mensajePassword.innerHTML = "password 2 es correcto";
+    }else{
+        mensajePassword.innerHTML = "password incorrecto";
+    }
 }
